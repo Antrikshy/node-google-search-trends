38e33a81127d218beaa8e24320307cc89ac58574
Antriksh Yadav <antrikshy@gmail.com>
2015-03-27 20:40:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var parseString = require('xml2js').parseString;
+
+var countryIds = require('countries.json');
+
+module.exports = function(localization, count, callback) {
+  var url = "http://www.google.com/trends/hottrends/atom/feed?pn=" + countryIds[localization];
+  
+  xmlFeedToJson(url, function(err, data) {
+    var allFeedItems = data.rss.channel['0'].item;
+    var toReturn = {};
+
+    var i = 0;
+    while (i < count) {
+      toReturn[i] = allFeedItems[i];
+      i++;
+    }
+
+    callback(err, toReturn);
+  });
+}
+
+function xmlFeedToJson(url, callback) {
+  var req = http.get(url, function(res) {
+    var xml = '';
+    
+    res.on('data', function(chunk) {
+      xml += chunk;
+    });
+
+    res.on('error', function(e) {
+      callback(e, null);
+    }); 
+
+    res.on('timeout', function(e) {
+      callback(e, null);
+    }); 
+
+    res.on('end', function() {
+      parseString(xml, function(err, result) {
+        callback(null, result);
+      });
+    });
+  });
+}
